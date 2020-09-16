5385e9a03602621960acdbc9924c72946e917f7c
Andres-XL <amovilla1103@gmail.com>
2020-09-15 19:11:25 -0500

    that was faster than i thought

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from "react-router-dom";
 import { LandingPage } from "./Pages/LandingPage";
 import { LoginPage } from "./Pages/LoginPage";
+import { TestQR } from "./Pages/TestQR";
 
 export class App extends Component {
     
@@ -10,24 +11,25 @@ export class App extends Component {
     }
     
     render() {
-        const { userData } = this.state;
+        // const { userData } = this.state;
         
-        if ( !userData ) {
-            return (
-                <LoginPage
-                    onSuccessCallback={( userData ) => this.setState(userData)}
-                />
-            )
-        }
-        
-        return (
-            <Switch>
-                <Route path="/" render={( props ) => {
-                    return (
-                        <LandingPage {...props} />
-                    )
-                }} />
-            </Switch>
-        );
+        return <TestQR />
+        // if ( !userData ) {
+        //     return (
+        //         <LoginPage
+        //             onSuccessCallback={( userData ) => this.setState(userData)}
+        //         />
+        //     )
+        // }
+        //
+        // return (
+        //     <Switch>
+        //         <Route path="/" render={( props ) => {
+        //             return (
+        //                 <LandingPage {...props} />
+        //             )
+        //         }} />
+        //     </Switch>
+        // );
     }
 }
